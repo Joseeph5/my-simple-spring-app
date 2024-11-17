fec7b7965c1a63453d2aac06fb835164deae9c7a
Youssef Benamor <youssef.beenamor@gmail.com>
2024-11-17 13:08:41 +0100

    update

---
+++ src/main/java/com/youssef/simpespringapp/SimpeSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.simpespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpeSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpeSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youssef/simpespringapp/SimpeSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.simpespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpeSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
